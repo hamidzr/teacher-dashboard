4815e63d175f77525c1505f4225fb21690862693
Hamid Zare <dellydela@gmail.com>
2018-07-18 15:44:42 -0500

    created groups component installed axios

---
+++ src/components/Groups.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="grp in groups" :key="grp.name">{{ grp.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'groups',
+  data() {
+    return {
+    }
+  }, // end of data
+  created() {
+    this.fetchGroups()
+  },
+  computed: {
+    groups() {return this.$store.state.groups}
+  },
+  methods: {
+    async fetchGroups() {
+      const endpoint = this.$store.state.SERVER_ADDRESS + '/api/groups'
+      let { data: groups } = await axios.get(endpoint, {
+        withCredentials: true,
+      })
+      this.$store.state.groups = groups;
+      console.log(groups);
+      return groups;
+    },
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Groups />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Groups from '@/components/Groups.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Groups
   }
 }
 </script>
