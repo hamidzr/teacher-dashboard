1f463cbef2d846cbeddc54d30f03ec2af7da97ce
Hamid Zare <dellydela@gmail.com>
2018-07-20 11:31:23 -0500

    load group data when editing a group assumes that we want direct linking to group editing

--- src/views/GroupChangePage.vue
+++ src/views/GroupChangePage.vue
@@ -5,13 +5,30 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import GroupForm from '@/components/GroupForm.vue'
+import groupMixin from '@/mixins/group'
 
 export default {
   name: 'groupChangePage',
   props: ['id'],
+  mixins: [groupMixin],
+
   components: {
     GroupForm,
+  },
+
+  created() {
+    if (this.hasGroup()) this.loadGroupData();
+  },
+
+  methods: {
+    ...mapActions(['createUser']),
+
+    hasGroup() {
+      return !!this.id;
+    },
+
   }
 
 }
