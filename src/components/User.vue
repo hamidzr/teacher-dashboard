5a4996464f8d495e99aeb84d079c75da86749aec
Hamid Zare <dellydela@gmail.com>
2018-07-31 15:37:25 -0500

    simple user deletion button for user component

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <li>username: {{ user.username }}, email: {{ user.email }}</li>
+    <li>username: {{ user.username }}, email: {{ user.email }}
+      <a href="#" ><i class="material-icons">edit</i></a>
+      <a href="#" @click.prevent="deleteUserConfirm()"><i class="material-icons">delete</i></a>
+    </li>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'user',
   props: ['user'],
+  data() {
+    return {
+      editing: false,
+    }
+  },
+
+  methods: {
+    ...mapActions(['deleteUser', 'editUser']),
+    async deleteUserConfirm() {
+      if (confirm(`are you sure you want to delete user ${this.user.username}?`)) {
+        return await this.deleteUser(this.user);
+      }
+    }
+  }
 }
 </script>
