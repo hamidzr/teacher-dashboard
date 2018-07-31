587ecf8b833b9a1a1eae621c0bea01f28112dd23
Hamid Zare <dellydela@gmail.com>
2018-07-31 13:31:53 -0500

    changed default group dataloading to always fetch from server instead of getting from store

--- src/mixins/group.js
+++ src/mixins/group.js
@@ -19,10 +19,15 @@ export default  {
     ...mapActions(['fetchGroup', 'fetchUsers']),
 
     // loads group data. assumes that there is such data
-    async loadGroupData() {
+    async loadGroupData(fromStore=false) {
       let group = this.getGroupById(this.id);
-      group = group ? group : await this.fetchGroup(this.id);
-      group.users = group.users ? group.users : await this.fetchUsers(this.id);
+      if (fromStore) {
+        group = group ? group : await this.fetchGroup(this.id);
+        group.users = group.users ? group.users : await this.fetchUsers(this.id);
+      } else {
+        group = await this.fetchGroup(this.id);
+        group.users = await this.fetchUsers(this.id);
+      }
       this.group = group;
       return group;
     },
