993520b3f91821ab45a5459b64679c5abfb03029
Hamid Zare <dellydela@gmail.com>
2018-08-07 16:38:12 -0500

    changed Users component to show as a table (created UserTableRow component)

---
+++ src/components/UserTableRow.vue
@@ -0,0 +1,19 @@
+<template>
+  <tr :id="htmlId()">
+    <td class="username" :contenteditable="editing">{{ user.username }}</td>
+    <td class="email" :contenteditable="editing">{{ user.email }}</td>
+    <td>
+      <a href="#" v-if="editing" @click.prevent="save"><i class="material-icons">check</i></a>
+      <a href="#" v-else @click.prevent="toggleEditing"><i class="material-icons">edit</i></a>
+      <a href="#" @click.prevent="deleteUserConfirm()"><i class="material-icons">delete</i></a>
+    </td>
+  </tr>
+</template>
+
+<script>
+import User from '@/components/User'
+
+export default {
+  extends: User
+}
+</script>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <ul>
-      <User v-for="user in users" :key="user.username" :user="user"/>
-    </ul>
+    <table class="centered stripped highlight responsive-table">
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <UserTableRow v-for="user in users" :key="user.username" :user="user"/>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import User from '@/components/User.vue'
+import UserTableRow from '@/components/UserTableRow.vue'
 
 export default {
   name: 'users',
   props: ['groupId', 'users'],
   components: {
-    User,
+    UserTableRow,
   },
 }
 </script>
