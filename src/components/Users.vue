e812d124b0dc99265acb912d01edd1bd6dd2e544
Hamid Zare <dellydela@gmail.com>
2018-08-08 13:14:41 -0500

    added inline user creation to users component

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
+    <div>
+      <!-- search bar and user creation bar -->
+      <button v-if="!isAddingUser" @click="toggleAddingUser">Create User</button>
+      <button v-else @click="addUser">Save User</button>
+    </div>
+    <div v-if="isAddingUser">
+      <div class="row">
+        <div class="col s4">
+          Username:
+          <div class="input-field inline">
+            <input v-model="newUser.username" type="text">
+            <!-- <label for="email_inline">Username</label> -->
+          </div>
+        </div>
+        <div class="col s4">
+          Email:
+          <div class="input-field inline">
+            <input v-model="newUser.email" type="email" class="validate">
+            <!-- <label for="email_inline">Email</label> -->
+          </div>
+        </div>
+        <div class="col s4">
+          Password:
+          <div class="input-field inline">
+            <input v-model="newUser.password" type="text">
+            <!-- <label for="email_inline">Password</label> -->
+          </div>
+        </div>
+      </div>
+
+    </div>
     <table class="centered stripped highlight responsive-table">
       <thead>
         <tr>
@@ -18,12 +49,48 @@
 
 <script>
 import UserTableRow from '@/components/UserTableRow.vue'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'users',
   props: ['groupId', 'users'],
+  data() {
+    return {
+      newUser: {
+        username: '',
+        email: '',
+        groupId: this.groupId,
+        password: '',
+      },
+
+      isAddingUser: false,
+    }
+  },
   components: {
     UserTableRow,
   },
+
+  methods: {
+    ...mapActions(['createUser']),
+    toggleAddingUser() {
+      this.isAddingUser = !this.isAddingUser;
+      this.resetNewUser();
+    },
+
+    resetNewUser() {
+      this.newUser = {
+        username: '',
+        email: '',
+        groupId: this.groupId,
+        password: '',
+      };
+    },
+
+    async addUser() {
+      this.isAddingUser = false;
+      await this.createUser(this.newUser);
+      this.newUser
+    }
+  }
 }
 </script>
