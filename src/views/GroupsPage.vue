59a322d5aadc0e5e13b61a77d23dd7d1fce55769
Hamid Zare <dellydela@gmail.com>
2018-08-09 16:44:09 -0500

    easy group creatoin on groups page

--- src/views/GroupsPage.vue
+++ src/views/GroupsPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h2>Your Groups
-      <router-link to="/groups/new">+</router-link>
+      <a href="#" @click.prevent="newGroup">+</a>
     </h2>
     <Groups />
   </div>
@@ -9,11 +9,32 @@
 
 <script>
 import Groups from '@/components/Groups.vue'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'groupsPage',
   components: {
     Groups
+  },
+
+  computed: {
+    groups() {return this.$store.state.groups}
+  },
+
+  methods: {
+    ...mapActions(['createGroup', 'updateGroup']),
+
+    async newGroup() {
+      const baseName = 'newGroup';
+      let suggestedName = baseName + '1';
+      let exists = name => !!this.groups.find(g => g.name === name);
+      while (exists(suggestedName)) {
+        let curNum = parseInt(suggestedName.match(/\d+/)[0]);
+        curNum++;
+        suggestedName = baseName + curNum;
+      }
+      if (confirm('Are you sure you want to create a new group?')) await this.createGroup(suggestedName);
+    }
   }
 }
 </script>
