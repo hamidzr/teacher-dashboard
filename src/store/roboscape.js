0f0fd203a785d13fa41c0c775957e40b9470dacf
Hamid Zare <dellydela@gmail.com>
2019-02-13 15:11:18 -0600

    WIP roboscapestore

--- src/store/roboscape.js
+++ src/store/roboscape.js
@@ -19,15 +19,14 @@ export default {
     },
 
     updateRobot(state, robot) {
-      let targetRobot = findRobot(state, robot._id);
+      let targetRobot = findRobot(state, robot.robotId);
       console.assert(targetRobot, 'robot is missing');
-      targetRobot.name = robot.name;
+      robotChangeableAttrs.forEach(att => {
+        if (robot[att] !== undefined) {
+          targetRobot[att] = robot[att];
+        }
+      })
       targetRobot.users = robot.users || [];
-    },
-
-    setRobotUsers(state, payload) {
-      const { robotId, users } = payload;
-      findRobot(state, robotId).users = users;
     },
 
     // update a single user
@@ -43,6 +42,12 @@ export default {
       })
     },
 
+    addRobotUser(state, user) {
+      let robot = findRobot(state, user.robotId);
+      if (!robot.users) robot.users = [];
+      robot.users.push(user);
+    },
+
     deleteRobotUser(state, user) {
       console.debug(`deleting ${user.username}`);
       let robot = findRobot(state, user.robotId);
@@ -50,18 +55,12 @@ export default {
       robot.users.splice(targetIndex, 1);
     },
 
-    addRobotUser(state, user) {
-      let robot = findRobot(state, user.robotId);
-      if (!robot.users) robot.users = [];
-      robot.users.push(user);
-    },
-
   },
 
   getters: {
     getRobotById(state) {
       return robotId => {
-        return state.robots.find(g => g._id === robotId)
+        return state.robots.find(r => r.robotId === robotId)
       }
     },
 
@@ -87,9 +86,8 @@ export default {
   },
 
   actions: {
-    // WARN probably shouldn't use the return values from actions as they are not a reference  to the store
     async fetchRobots(context) {
-      const endpoint = context.state.SERVER_ADDRESS + '/api/robots'
+      const endpoint = context.state.SERVER_ADDRESS + '/api/roboscape/robots'
       let { data: robots } = await axios.get(endpoint, {
         withCredentials: true,
       })
@@ -111,20 +109,7 @@ export default {
       return robot;
     },
 
-    async fetchUsers(context, robotId) { // gets robot details
-      if (!robotId || robotId.length !== 24) throw new Error('invalid robot id', robotId);
-      console.log('fetching users', robotId);
-      const endpoint = context.state.SERVER_ADDRESS + `/api/robots/${robotId}/members`
-      let { data: users } = await axios.get(endpoint, {
-        withCredentials: true,
-      })
-      let targetRobot = context.getters.getRobotById(robotId);
-      console.assert(targetRobot.name, 'invalid robot');
-      context.commit('setRobotUsers', {robotId, users});
-      return users;
-    },
-
-    async createUser(context, user) {
+    async addUser(context, user) {
       console.debug(`creating user ${user.username}`);
       if (!user || !user.username || !user.robotId || !user.email || !user.password) throw new Error(`missing user data, ${user.username}`);
       const endpoint = context.state.SERVER_ADDRESS + `/api/robots/${user.robotId}/members`
@@ -151,17 +136,6 @@ export default {
       return response.data;
     },
 
-    async deleteUser(context, user) {
-      console.log(`deleting user ${user.username}`);
-      if (!user || !user.username || !user.email) throw new Error(`missing user data, ${user}}`);
-      const endpoint = context.state.SERVER_ADDRESS + `/api/robots/${user.robotId}/members/${user._id}`;
-      let response = await axios.delete(endpoint, {
-        withCredentials: true
-      })
-      context.commit('deleteRobotUser', user);
-      return response.data;
-    },
-
     async updateRobot(context, robot) {
       console.log(`updating robot ${robot.name}`);
       if (!robot || !robot.name) throw new Error(`missing robot data, ${robot}}`);
@@ -171,16 +145,6 @@ export default {
       })
       context.commit('patchRobot', robot);
       return response.data;
-    },
-
-    async deleteRobot(context, robot) {
-      console.log('deleting robot', robot.name);
-      const endpoint = context.state.SERVER_ADDRESS + '/api/robots/' + robot._id;
-      let { data: resp } = await axios.delete(endpoint, {
-        withCredentials: true
-      })
-      context.commit('deleteRobot', robot);
-      return resp;
     },
 
   },
