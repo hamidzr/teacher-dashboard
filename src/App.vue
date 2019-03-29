6ae9728c1d61ab841bc5b446cf68b9b0de5af9fa
Hamid Zare <dellydela@gmail.com>
2019-03-29 17:25:55 -0500

    use main element instead of div

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <Header />
-      <div class="container">
+      <main class="container">
         <router-view/>
-      </div>
+      </main>
     <Footer />
   </div>
 </template>
