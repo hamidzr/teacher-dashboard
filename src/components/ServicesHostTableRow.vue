b064dd35f04bbd4d3cf7b11bac024f9a7f2e285b
Brian Broll <brian.broll@gmail.com>
2020-04-23 15:40:19 -0500

    Add validation of custom services

--- src/components/ServicesHostTableRow.vue
+++ src/components/ServicesHostTableRow.vue
@@ -1,11 +1,10 @@
 <template>
   <tr :id="elementId()">
     <td class="name">{{ servicesHost.categories[0] }}</td>
-    <td :class="{ editable: editing, value: true }" :contenteditable="editing">{{ servicesHost.url }}</td>
+    <td>{{ servicesHost.url }}</td>
     <td>
-      <a href="#" v-if="editing" @click.prevent="save"><i class="material-icons" title="Save">check</i></a>
-      <a href="#" v-if="editing" @click.prevent="cancelEditing"><i class="material-icons" title="Cancel">close</i></a>
       <a href="#" @click.prevent="deleteServicesHostConfirm()"><i class="material-icons" title="Delete">delete</i></a>
+      <a href="#" v-if="isInvalid" @click.prevent="invalidURL()"><i class="material-icons" style="color: red" title="NetsBlox services not found at the given URL. Is the URL correct?">error</i></a>
     </td>
   </tr>
 </template>
