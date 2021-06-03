3facd80a7fbd520d1e9f42b169e5c84115ead88a
Brian Broll <brian.broll@gmail.com>
2021-06-03 15:51:23 -0500

    Fix newline character...

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -6,7 +6,8 @@
         <a style="position: relative"
             href="#" v-show="!isAddingUser"
             @click.prevent="uploadCSV"
-            title="Create members from CSV &#010;(columns: username,email,password)">
+            title="Create members from CSV
+(columns: username,email,password)">
             <i style="top: 2px; position: absolute;" class="material-icons" >arrow_circle_up</i>
         </a>
         <a href="#" v-show="isAddingUser" @click.prevent="addUser"><i class="material-icons" title="Save user">check</i></a>
